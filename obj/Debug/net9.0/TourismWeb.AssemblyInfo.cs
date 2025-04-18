c1cfd5fe69c39a0a0c004b0baf3aed951cd4c7f3
NguyenNhuQuynh <102230099@sv1.dut.udn.vn>
2025-04-19 01:47:28 +0700

    Lưu lại thay đổi trước khi merge

--- obj/Debug/net9.0/TourismWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/TourismWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f7895b9eb60a458e53af02fa186812b242e77d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d152a97dc3221858084103d7c9ce1648e148cc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
