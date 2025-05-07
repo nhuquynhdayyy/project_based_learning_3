a44ebbe6bc74e7a5504c70a2e704cea5d394790c
NguyenNhuQuynh <102230099@sv1.dut.udn.vn>
2025-05-07 22:14:01 +0700

    ver 22:13 05.07.2025

--- obj/Debug/net9.0/TourismWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/TourismWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52abe9f2a80e39abc31941ca78503b16ccad90eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e436a63a632f6e5b984f427c6a1f8eb75288b751")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
